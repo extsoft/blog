91ffa0a6497554f08801b0ee9770e623cb5f3f7f
Dmytro Serdiuk <dmytro.serdiuk@gmail.com>
2019-01-14 21:33:18 +0200

    Allow drafts

--- preview.sh
+++ preview.sh
@@ -2,7 +2,7 @@
 set -e
 case $1 in
     check)      jekyll clean && jekyll build && jekyll doctor ;;
-    preview)    jekyll serve --watch ;;
+    preview)    jekyll serve --watch --drafts ;;
     *)          echo "'check' or 'preview' is allowed"; exit -1 ;;
 esac
 
